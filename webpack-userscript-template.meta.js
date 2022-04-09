5eb5f042c65ad8441bfc4dd6593ca03689c376d8
crashmax-dev <crashmax.dev@gmail.com>
2022-04-09 09:03:20 +0000

    Deploying to gh-pages from @ crashmax-dev/twitch-chat-translate@f61c36497377141ef890b6b1416ea03c87b6db90 🚀

---
+++ webpack-userscript-template.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        webpack-userscript-template
+// @version     0.0.1
+// @author      Vitalij Ryndin
+// @description webpack-userscript-template
+// @homepage    https://crashmax-off.github.io/webpack-userscript-template/
+// @include     /^(http|https)://(example.com|example.org|example.edu).*$/
+// ==/UserScript==
