1225488ea791c43826adb1a9e54a036601b2af88
crashmax-dev <crashmax.dev@gmail.com>
2022-04-09 12:23:26 +0000

    Deploying to gh-pages from @ crashmax-dev/twitch-chat-translate@b9dcf1bd941283bd2b0b3943016b13fec3c69665 🚀

--- twitch-chat-translation.meta.js
+++ twitch-chat-translation.meta.js
@@ -4,5 +4,5 @@
 // @author      Vitalij Ryndin
 // @description twitch-chat-translation
 // @homepage    https://crashmax-off.github.io/twitch-chat-translation/
-// @include     /^(http|https)://(example.com|example.org|example.edu).*$/
+// @include     /^(http|https)://(www.twitch.tv).*$/
 // ==/UserScript==
