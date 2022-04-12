be605d21c681590ee429302493426fe9108c1577
crashmax-dev <crashmax.dev@gmail.com>
2022-04-12 09:28:57 +0000

    Deploying to gh-pages from @ crashmax-dev/twitch-chat-translation@2a0e6774ff6ef133f329b9b699a7dea6479d6e06 🚀

--- twitch-chat-translation.meta.js
+++ twitch-chat-translation.meta.js
@@ -3,9 +3,9 @@
 // @version     0.0.1
 // @author      Vitalij Ryndin
 // @description twitch-chat-translation
-// @homepage    https://crashmax-dev.github.io/twitch-chat-translation/GM_config.js
+// @homepage    https://crashmax-dev.github.io/twitch-chat-translation/
 // @include     /^(http|https)://(www.twitch.tv|canary.twitch.tv|release.twitch.tv).*$/
-// @require     https://crashmax-dev.github.io/twitch-chat-translation/GM_config.jsGM_config.js
+// @require     https://crashmax-dev.github.io/twitch-chat-translation/GM_config.js
 // @grant       GM_registerMenuCommand
 // @grant       GM_getValue
 // @grant       GM_setValue
