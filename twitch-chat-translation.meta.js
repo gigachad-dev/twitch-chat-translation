a6086c963eeec7cdc92ae55a8c2fec3d6d6842e6
crashmax-dev <crashmax.dev@gmail.com>
2022-09-06 13:23:14 +0000

    Deploying to gh-pages from @ gigachad-dev/twitch-chat-translation@b92f2628f117bfc93359f5d2842eb2f857d5023f 🚀

---
+++ twitch-chat-translation.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        twitch-chat-translation
+// @version     2.0.0
+// @homepage    https://gigachad-dev.github.io/twitch-chat-translation/
+// @description twitch-chat-translation
+// @author      Vitalij Ryndin
+// @match       https://*.twitch.tv/*
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addStyle
+// @grant       GM_registerMenuCommand
+// @grant       GM_log
+// @updateURL   https://gigachad-dev.github.io/twitch-chat-translation/twitch-chat-translation.meta.js
+// @downloadURL https://gigachad-dev.github.io/twitch-chat-translation/twitch-chat-translation.user.js
+// ==/UserScript==
